43bc3028c0aacd3073fa5ca75cce33e81765bfe7
Hrithik.s.raj <84892078+Hrithik-s-Raj@users.noreply.github.com>
2022-09-22 10:05:43 +0530

    sanity

--- sanity.ts
+++ sanity.ts
@@ -1,3 +1,5 @@
+//Sanity connection file
+
 import { createClient } from "next-sanity";
 import createImageUrlBuilder from "@sanity/image-url";
 
@@ -13,3 +15,5 @@ export const sanityClient = createClient(config);
 //Helper function for fetching the image url from sanity
 export const urlFor = (source: any) =>
   createImageUrlBuilder(config).image(source);
+
+  console.log("Hllo from sanity");
